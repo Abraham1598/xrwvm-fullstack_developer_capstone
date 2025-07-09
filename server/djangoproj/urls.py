0d6bc54b17caa5ce431cb037adf565a54e13e8e1
Abraham1998 <wichysanchz@gmail.com>
2025-07-09 19:54:23 -0400

    Adding temporary changes to Github

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,6 +22,8 @@ Including another URLconf
 """
 
 urlpatterns = [
+    path('postreview/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
+    path('dealer/<int:dealer_id>',TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
